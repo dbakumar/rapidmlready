8547c1f06e27509349f57a80da13de829deddbc4
Vijay <dbakumar@gmail.com>
2026-04-17 15:27:16 -0500

    workflow defined

--- examples/diabetes_nephropathy/attrition_20260415_170301.sql
+++
@@ -1,6 +0,0 @@
--- Attrition summary (run after loading study.sql output into temp/result table)
-SELECT
-  COUNT(*) AS total_rows,
-  COUNT(DISTINCT person_id) AS unique_people,
-  SUM(CASE WHEN outcome_label = 1 THEN 1 ELSE 0 END) AS positive_rows
-FROM study_result;
--- examples/diabetes_nephropathy/data_quality_report_20260415_170301.sql
+++
@@ -1,6 +0,0 @@
--- Data quality checks for study_result
-SELECT 'null_person_id' AS check_name, COUNT(*) AS issue_rows FROM study_result WHERE person_id IS NULL
-UNION ALL
-SELECT 'null_index_date' AS check_name, COUNT(*) AS issue_rows FROM study_result WHERE index_date IS NULL
-UNION ALL
-SELECT 'label_out_of_range' AS check_name, COUNT(*) AS issue_rows FROM study_result WHERE outcome_label NOT IN (0, 1);
